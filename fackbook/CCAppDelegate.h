85b200ae3e3159143db3a3a2338cefe29f6acbfd
Alex Yao <kukathe@gmail.com>
2012-06-20 14:45:49 +0800

    Initial Commit

---
+++ fackbook/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  fackbook
+//
+//  Created by Alex Yao on 20/6/12.
+//  Copyright (c) 2012 alex@thecellcity.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fackbook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fackbook
+//
+//  Created by Alex Yao on 20/6/12.
+//  Copyright (c) 2012 alex@thecellcity.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
